518af94b202efed16e3b0084dbc5aed4f825bea1
panayotsky.dev <panayotsky.dev@gmail.com>
2022-12-14 22:23:28 +0200

    asd

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,8 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  let hotInHere = document.querySelector('.hot')
+let textInside = document.querySelector('.hot').textContent
+  hotInHere.innerHTML = textInside + '🔥'
+  
 });
